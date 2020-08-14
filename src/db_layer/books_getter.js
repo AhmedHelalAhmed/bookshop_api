6ef72a81ef461afe7564a057a85ed2e471b5e762
unknown <ammaryaser293@gmail.com>
2020-08-14 19:26:42 +0200

    manual test

--- src/db_layer/books_getter.js
+++ src/db_layer/books_getter.js
@@ -1,13 +1,16 @@
 const db_client = require('./db_config.js');
 
 function get_books(callback) {
+    console.log('books_getter 5');
     db_client.query(`SELECT * FROM books`,
         (err, res) => {
+            console.log('books_getter 8');
             if (err) {
+                console.log('books_getter 9');
                 callback(err, null);
                 return;
             }
-
+            console.log('books_getter 14');
             callback(null, res['rows']);
         });
 }
